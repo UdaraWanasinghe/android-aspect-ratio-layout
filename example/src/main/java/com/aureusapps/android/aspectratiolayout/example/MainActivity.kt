5da05f8ffac01d8ed256b2b8701520b9cd3c6ef7
Udara Wanasinghe <udara.developer@gmail.com>
2022-11-02 22:02:31 +0530

    first commit

---
+++ example/src/main/java/com/aureusapps/android/aspectratiolayout/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aureusapps.android.aspectratiolayout.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aureusapps.android.aspectratiolayout.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
